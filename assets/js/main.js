f294930f21ca93c6763229d8dbc9d7509880177f
Pankaj Kumar <pkthapliyal101@gmail.com>
2023-06-02 19:56:14 +0530

    Project-final21

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,71 +91,43 @@ modalCloses.forEach((modalCloses) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiperPortfolio = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: true,
+// let swiperPortfolio = new Swiper(".portfolio__container", {
+//     cssMode: true,
+//     loop: true,
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
 
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper(".testimonial__container", {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
+// /*==================== TESTIMONIAL ====================*/
+// let swiperTestimonial = new Swiper(".testimonial__container", {
+//     loop: true,
+//     grabCursor: true,
+//     spaceBetween: 48,
 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
 
-    breakpoints: {
-        568: {
-            slidesPerView: 2,
-        },
-    },
-});
+//     breakpoints: {
+//         568: {
+//             slidesPerView: 2,
+//         },
+//     },
+// });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
-// function scrollActive() {
-//   const scrollY = window.pageYOffset;
 
-//   sections.forEach((current) => {
-//     const sectionHeight = current.offsetHeight;
-//     const sectionTop = current.offsetTop - 50;
-//     sectionId = current.getAttribute("id");
-
-//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//       document
-//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
-//         .classList.add("active-link");
-//     } else {
-//       document
-//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
-//         .classList.remove("active-link");
-//     }
-//   });
-// }
-// window.addEventListener("scroll", scrollActive);
-/*==================== CHANGE BACKGROUND HEADER ====================*/
-
-// function scrollHeader() {
-//   const nav = document.getElementById("header");
-//   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-//   if (this.scrollY >= 80) nav.classList.add("scroll-header");
-//   else nav.classList.remove("scroll-header");
-// }
-// window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
 
@@ -183,28 +155,6 @@ const getCurrentTheme = () =>
 const getCurrentIcon = () =>
     themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
-// We validate if the user previously chose a topic
-
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-//     darkTheme
-//   );
-//   themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-//     iconTheme
-//   );
-// }
-
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener("click", () => {
-//   // Add or remove the dark / icon theme
-//   document.body.classList.toggle(darkTheme);
-//   themeButton.classList.toggle(iconTheme);
-//   // We save the theme and the current icon that the user chose
-//   localStorage.setItem("selected-theme", getCurrentTheme());
-//   localStorage.setItem("selected-icon", getCurrentIcon());
-// });
-
 
 
 // testing js
@@ -224,15 +174,3 @@ window.onscroll = () => {
     navbar.classList.remove("active");
 };
 
-// Dark Mode
-let darkmode = document.querySelector("#darkmode");
-
-darkmode.onclick = () => {
-    if (darkmode.classList.contains("bx-moon")) {
-        darkmode.classList.replace("bx-moon", "bx-sun");
-        document.body.classList.add("active");
-    } else {
-        darkmode.classList.replace("bx-sun", "bx-moon");
-        document.body.classList.remove("active");
-    }
-};
